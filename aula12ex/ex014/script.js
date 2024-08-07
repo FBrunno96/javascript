6a18568ff6ad42051e8e4b79c4ff88a8d40e4132
Brunno Faissal <114701544+FBrunno96@users.noreply.github.com>
2024-08-07 18:42:11 -0300

    new commit

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 12
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        msg.innerHTML += '<p>Bom Dia!</p>'
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        msg.innerHTML += '<p>Boa Tarde!</p>'
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        msg.innerHTML += '<p>Boa noite!</p>'
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#515154'
+    }
+}
